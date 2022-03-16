af076f17d6312c4e1e47a862ca3c3bde482b1111
bot4ol <87345675+bot4ol@users.noreply.github.com>
2022-03-16 20:15:25 +0300

    Update lightshot.js

--- lightshot.js
+++ lightshot.js
@@ -11,7 +11,6 @@ function getRandomURL() {
 RandomURL=getRandomURL()
 var linkToPhoto=RandomURL.link(RandomURL)
 document.write(linkToPhoto)
-document.write("ЛОДКА ПРИВЕТ!!!")
 if (getRandomURL=="https://prnt.sc/148822"){
     console.log("НИХУЯ СЕБЕ")   
 }
