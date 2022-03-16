bcd91605cf9800a7de31df370d8b7d96a7fe770e
bot4ol <87345675+bot4ol@users.noreply.github.com>
2022-03-16 15:59:27 +0300

    Add files via upload

--- lightshot.js
+++ lightshot.js
@@ -1,12 +1,12 @@
-function getRandomURL(){
-    a=String(Math.floor(Math.random()*9))
-    b=String(Math.floor(Math.random()*9))
-    c=String(Math.floor(Math.random()*9))
-    d=String(Math.floor(Math.random()*9))
-    e=String(Math.floor(Math.random()*9))
-    f=String(Math.floor(Math.random()*9))
-    out= "https://prnt.sc/"+a+b+c+d+e+f
-    return(out)
+function getRandomURL() {
+    chrs = 'abdehkmnpswxzABDEFGHKMNPQRSTWXZ123456789';
+    var str = '';
+    for (var i = 0; i < 6; i++) {
+        var pos = Math.floor(Math.random() * chrs.length);
+        str += chrs.substring(pos,pos+1);
+    }
+    out="https://prnt.sc/"+str
+    return out;
 }
 RandomURL=getRandomURL()
 var linkToPhoto=RandomURL.link(RandomURL)
